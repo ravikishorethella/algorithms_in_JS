ccb3ffc43cda3d614ef5dc593741bd70dbdb8e3b
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-06-10 19:10:16 -0400

    Update Plus one.js

--- Array & String/Plus one.js
+++ Array & String/Plus one.js
@@ -2,3 +2,11 @@
 // link to the problem
 // https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1148/
 
+var plusOne = function(digits) {
+  for(var i = digits.length - 1; i >= 0; i--){
+    if(++digits[i] > 9) digits[i] = 0;
+    else return digits;
+  }
+  digits.unshift(1);
+  return digits;
+};
