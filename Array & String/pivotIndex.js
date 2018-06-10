5db3398cb848c1e57e60c41a78d0e35518c94bec
ravikishorethella <ravikishorethella@gmail.com>
2018-06-10 17:19:03 -0400

    initial commit

---
+++ Array & String/pivotIndex.js
@@ -0,0 +1,23 @@
+// find the pivot index of the array.
+// The pivot index is the index where the sum of the numbers to the left of the index
+// is equal to the sum of the numbers to the right of the index
+
+// If the leftSum is equal to the rightSum then return the index else return -1
+
+// METHOD 1: Brute-force
+
+var pivotIndex = function(nums) {
+  var left, right, leftSum, rightSum;
+  for (let i = 0; i < nums.length; i++) {
+    left = nums.slice(0, i);
+    right = nums.slice(i + 1);
+    leftSum = left.reduce((total, num) => total + num, 0);
+    rightSum = right.reduce((total, num) => total + num, 0);
+    if (leftSum === rightSum) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+console.log(pivotIndex([1, 7, 3, 6, 5, 6]));
